6340d176ccdb41b693f6b5833b3ae683b105800d
Spet77 <108152876+spet77@users.noreply.github.com>
2022-06-24 21:31:30 -0700

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+// google analytics measurement id
+const GA_TRACKING_ID = "";
+
+const META = {
+	title: "NextJS ChakraUI Template | Hazim Arafa",
+	lang: "en-us",
+	description:
+		"Boilerplate built to scale containing Typescript + NextJS + ChakraUI + Google Analytics + ESLint + Jest + Styled Components + Icons",
+	url: "https://boilerplate.hazim.tech",
+	image: "/logo.png",
+	tags: ["nextjs", "typescript", "chakraui", "google analytics", "eslint", "jest", "styled components", "icons"]
+};
+
+export { GA_TRACKING_ID, META };
---
+++ src/pages/_app.tsx
@@ -0,0 +1,42 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Footer from "@components/footer";
+import Header from "@components/header";
+import theme from "@styles/theme";
+import { pageview } from "@lib/gtag";
+import { META } from "config";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: unknown) => {
+      // @ts-ignore
+      window.gtag("config", GA_TRACKING_ID, {
+        page_location: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Head>
+        <title>{META.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Header />
+
+        <Component {...pageProps} />
+
+        <Footer />
+      </ChakraProvider>
+    </>
+  );
+}
